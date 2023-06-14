203fc7e403468a62c2795d4ec8eb25358691a121
drashti-d-7span <drashti.d@7span.com>
2023-06-14 17:31:59 +0530

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
         'secondary color': '#1E1E1E',
         'whitecolor': 'rgba(255, 255, 255, 0.8);',
         'orange-400': '#FF4300',
-        'orange-500':'#FF6400',
+        'orange-500': '#FF6400',
+        'blackcolor': '#18171A'
 
       },
 
@@ -19,7 +20,9 @@ module.exports = {
 
       backgroundImage: {
         'hero': "url('/Images/hero-bg-image.jpg')",
-        'GradientColor': 'linear-gradient(90deg, #FF4300 -6.33%, #FF6400 103.92%)'
+        'GradientColor': 'linear-gradient(90deg, #FF4300 -6.33%, #FF6400 103.92%)',
+        'classes':"url('/Images/classes-bg-img.jpg')" ,
+        'footer': "url('/Images/footer-bg-img.jpg')" ,
       },
 
       
